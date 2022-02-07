0e3a86a2a0246f2e81e87389fd81a25e6ba79c66
Magic_Vel <magic.valentyn@gmail.com>
2022-02-07 14:15:14 +0200

    transform

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -8,17 +8,30 @@ class MarvelService {
         let res = await fetch(url);
 
         if (!res.ok) {
-            throw new Error(`Cloud not fetch ${url}, status: ${res.status}`);
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
 
         return await res.json();
     }
 
-    getAllCharacters =() => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=210%20&${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter)
     }
-    getCharacter =(id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?${this.apiKey}`);
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'Прости, Я ничего о нём не знаю:(',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 }
 
